688a59145061c0b887d915e1951a461790606193
bala-amypo <bala@amypo.in>
2025-12-20 14:52:55 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,45 @@
+package com.example.demo.model;
+
+
+import jakarta.persistence.*;
+import java.sql.Timestamp;
+
+
+@Entity
+public class Employee {
+
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+
+private String fullName;
+
+
+@Column(unique = true)
+private String email;
+
+
+private String department;
+private String jobTitle;
+private Boolean active = true;
+
+
+private Timestamp createdAt;
+private Timestamp updatedAt;
+
+
+@PrePersist
+public void onCreate() {
+createdAt = new Timestamp(System.currentTimeMillis());
+}
+
+
+@PreUpdate
+public void onUpdate() {
+updatedAt = new Timestamp(System.currentTimeMillis());
+}
+
+
+}
