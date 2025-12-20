9a156f4a16387108a51f0bfeebefd590edeb01dc
bala-amypo <bala@amypo.in>
2025-12-20 14:15:41 +0000

    User commit

---
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.demo.exception;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidation(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors()
+                .forEach(e -> errors.put(e.getField(), e.getDefaultMessage()));
+        return errors;
+    }
+}
--- src/main/java/com/example/demo/service/StudentService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.StudentEntity;
-public interface StudentService{
-       StudentEntity postData(StudentEntity stu);
-
-}
