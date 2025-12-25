1ed5fd3a6ba6a1bb60443ae1d5512a4a9a73037c
bala-amypo <bala@amypo.in>
2025-12-25 08:18:53 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/EmployeeSkillServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.EmployeeSkillService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeSkillServiceImpl implements EmployeeSkillService {
+}
