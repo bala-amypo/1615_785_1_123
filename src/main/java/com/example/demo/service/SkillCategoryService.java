36c40d2638abe1d539f436462042d290d3ee53ff
bala-amypo <bala@amypo.in>
2025-12-25 08:45:52 +0000

    User commit

--- src/main/java/com/example/demo/service/SkillCategoryService.java
+++ src/main/java/com/example/demo/service/SkillCategoryService.java
@@ -1,4 +1,5 @@
 package com.example.demo.service;
 
 public interface SkillCategoryService {
+    String getCategoryName(Long id);
 }
--- src/main/java/com/example/demo/service/impl/SkillCategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SkillCategoryServiceImpl.java
@@ -1,8 +1,13 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.service.SkillCategoryService;
 import org.springframework.stereotype.Service;
+import com.example.demo.service.SkillCategoryService;
 
 @Service
 public class SkillCategoryServiceImpl implements SkillCategoryService {
+
+    @Override
+    public String getCategoryName(Long id) {
+        return "Default Skill Category";
+    }
 }
