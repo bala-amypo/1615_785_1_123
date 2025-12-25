636ece4c35b7b454b81b0bdce93e63e5c3de5c6e
bala-amypo <bala@amypo.in>
2025-12-25 08:22:31 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/SearchQueryServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.SearchQueryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchQueryServiceImpl implements SearchQueryService {
+}
