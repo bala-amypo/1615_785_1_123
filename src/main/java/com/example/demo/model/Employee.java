0b7df7fec9da155db1a7986fcd1a6e4a3007e4ef
bala-amypo <bala@amypo.in>
2025-12-25 04:45:53 +0000

    User commit

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -2,18 +2,18 @@ package com.example.demo.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
 @Table(name = "employees")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String fullName;
     private String email;
     private String department;
