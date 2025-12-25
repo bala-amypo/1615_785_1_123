8f4586f11bf9bc8c44b32600656613af1a9d6c7d
bala-amypo <bala@amypo.in>
2025-12-25 08:33:53 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/SkillCategoryServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.SkillCategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SkillCategoryServiceImpl implements SkillCategoryService {
+}
