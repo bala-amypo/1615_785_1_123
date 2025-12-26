55f784e7a6445517f486409027146d2cea86f0a0
bala-amypo <bala@amypo.in>
2025-12-26 05:46:29 +0000

    User commit

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -1,9 +1,7 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.*;
+import com.example.demo.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.*;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    Optional<Employee> findByEmail(String email);
 }
--- src/main/java/com/example/demo/service/impl/EmployeeSkillServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EmployeeSkillServiceImpl.java
@@ -1,11 +1,16 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.model.Employee;
 import com.example.demo.model.EmployeeSkill;
+import com.example.demo.model.Skill;
+import com.example.demo.repository.EmployeeRepository;
 import com.example.demo.repository.EmployeeSkillRepository;
+import com.example.demo.repository.SkillRepository;
 import com.example.demo.service.EmployeeSkillService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
 public class EmployeeSkillServiceImpl implements EmployeeSkillService {
 
